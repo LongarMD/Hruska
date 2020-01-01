4e782594108aa900cda82af3d584fce1041b7e70
LongarMD <mark.longar@gmail.com>
2020-01-01 16:43:07 +0100

    Adds panel view

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 from kivy.config import Config
 
 from kivymd.app import MDApp
+from kivy.uix.widget import Widget
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import *
 
 import style
+Builder.load_string(style.root_kv)
 
 
 class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
+        self.add_widget(DrinkPanel())
+
+    def on_pre_enter(self, *args):
+        print('yeet')
+
+
+class SettingsScreen(Screen):
     pass
 
 
-class DrinksScreen(Screen):
-    pass
+class DrinkPanel(Widget):
+    def __init__(self, **kwargs):
+        super(DrinkPanel, self).__init__(**kwargs)
+        self.size = HruskaApp.get_size(0.45, 0.8)
+        self.draw()
+
+    def draw(self):
+        with self.canvas:
+            Color(1, 1, 1, 1)
+            Rectangle(pos=self.pos, size=self.size)
 
 
 class HruskaApp(MDApp):
@@ -24,11 +48,26 @@ class HruskaApp(MDApp):
         Config.set('graphics', 'resizable', False)
         Config.set('graphics', 'width', '1024')
         Config.set('graphics', 'height', '600')
+        Config.write()
 
         super().__init__(**kwargs)
 
     def build(self):
-        return Builder.load_string(style.root_kv)
+        sm = ScreenManager()
+        main_screen, settings_screen = MainScreen(name='main'), SettingsScreen(name='settings')
+
+        sm.add_widget(main_screen)
+        sm.add_widget(settings_screen)
+
+        return sm
+
+    @staticmethod
+    def get_size(x, y, orientation_x='h', orientation_y='v'):
+        window_size = Window.size
+        new_x = window_size[0] * x if orientation_x == 'h' else window_size[1] * x
+        new_y = window_size[1] * y if orientation_y == 'v' else window_size[0] * y
+
+        return new_x, new_y
 
 
 if __name__ == "__main__":
--- style.py
+++ style.py
@@ -1,39 +1,28 @@
 root_kv = """
-ScreenManager:
-    MainScreen:
-    DrinksScreen:
+#: import SlideTransition kivy.uix.screenmanager.SlideTransition
 
-<MainScreen>
-    name: 'main_screen'
+<MainScreen>:
     AnchorLayout:
         anchor_x: 'right'
         anchor_y: 'top'
         MDIconButton:
             icon: 'settings'
             text_color: [1, 1, 1, 1]
-            
-    AnchorLayout:
-        anchor_x: 'center'
-        anchor_y: 'center'
-        
-    
-        FloatLayout:
-            MDRectangleFlatButton:
-                text: "Rum & Coke"
-                pos_hint: {"center_x": .5, "center_y": .5}
-                font_size: 50
-                size: 200, 120
+            size: app.get_size(0.15, 0.15, 'v', 'v')
+            on_release:
+                app.root.transition = SlideTransition(direction='down')
+                app.root.current = 'settings'
 
     
-<DrinksScreen>
-    name: 'drinks_screen'
+<SettingsScreen>:
     BoxLayout:
         orientation: "vertical"
     
         FloatLayout:
-    
             MDRaisedButton:
                 text: "Go back"
-                on_release: app.root.current = 'main_screen'
                 pos_hint: {"center_x": .5, "center_y": .5}
+                on_release:
+                    app.root.transition = SlideTransition(direction='up')
+                    root.manager.current = 'main'
 """
